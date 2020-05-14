c2852da5d3c48b2c043fba1d701a2ad939064155
Bima Panji <bimapanji5@gmail.com>
2020-05-14 23:59:54 +0700

    Reset, remove watch and home from commit

--- src/components/vtubers/CardList.jsx
+++ src/components/vtubers/CardList.jsx
@@ -10,20 +10,22 @@ const Table = ({ columns, data }) => {
           data.map(vtuberdata =>
               <div className="vtubers-cardlist-card">
                 <img src={vtuberdata.channelIcon} className="vtubers-cardlist-card-image" alt={vtuberdata.channelName} />
-                <div className="vtubers-cardlist-card-title">{vtuberdata.channelName}</div>
-                <div className="vtubers-cardlist-card-subtitle">Published Date</div>
-                <div className="vtubers-cardlist-card-debut">{vtuberdata.publishDate}</div>
-                <div className="vtubers-cardlist-card-subtitle">Subscribers</div>
-                <div className="vtubers-cardlist-card-debut">{vtuberdata.subscriber}</div>
-                <div className="vtubers-cardlist-card-subscribebuttoncontainer">
-                  <a
-                    href={`https://youtube.com/channel/${vtuberdata.id}?sub_confirmation=1`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="vtubers-cardlist-card-subscribebutton"
-                  >
-                    Subscribe
-                  </a>
+                <div className="vtubers-cardlist-card-detail">
+                  <div className="vtubers-cardlist-card-title">{vtuberdata.channelName}</div>
+                  <div className="vtubers-cardlist-card-subtitle">Published Date</div>
+                  <div className="vtubers-cardlist-card-debut">{vtuberdata.publishDate}</div>
+                  <div className="vtubers-cardlist-card-subtitle">Subscribers</div>
+                  <div className="vtubers-cardlist-card-debut">{vtuberdata.subscriber}</div>
+                  <div className="vtubers-cardlist-card-subscribebuttoncontainer">
+                    <a
+                      href={`https://youtube.com/channel/${vtuberdata.id}?sub_confirmation=1`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="vtubers-cardlist-card-subscribebutton"
+                    >
+                      Subscribe
+                    </a>
+                  </div>
                 </div>
               </div>
           )
